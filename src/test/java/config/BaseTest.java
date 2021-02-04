27748fbb6e7fd68fd366f8fe4d0045439ccc70b2
Kydster <kydster95@gmail.com>
2021-02-04 22:56:28 +0200

    added the BaseTest class

---
+++ src/test/java/config/BaseTest.java
@@ -0,0 +1,12 @@
+package config;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class BaseTest {
+
+  @BeforeAll
+  public static void setup() {
+    Configuration.startMaximized = true;
+  }
+}
